9b3fec37165296a4735e42a5b1c9a4d791507d9c
Ryan Christensen <ryan.christensen20@gmail.com>
2017-05-25 13:29:33 -0700

    Adding the ability to read high score from file.

--- game_stats.py
+++ game_stats.py
@@ -1,6 +1,5 @@
 class GameStats():
 	"""Track statistics for Alien Invasion."""
-
 	def __init__(self, ai_settings):
 		"""Initialize statistics."""
 		self.ai_settings = ai_settings
@@ -9,11 +8,19 @@ class GameStats():
 		# Start Alien Invasion in an inactive state.
 		self.game_active = False
 
-		# High score should never be reset.
-		self.high_score = 0
+		# High score is loaded from file, unless file doesn't exist.
+		try:
+			self.read_high_score()
+		except FileNotFoundError:
+			self.high_score = 0
 
 	def reset_stats(self):
 		"""Initialize statistics that can change during the game."""
 		self.ships_left = self.ai_settings.ship_limit
 		self.score = 0
 		self.level = 1
+
+	def read_high_score(self):
+		"""Read high score from textfile."""
+		with open('high_score.txt') as file_object:
+			self.high_score = int(file_object.read())
