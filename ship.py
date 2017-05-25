9c592b44bfde34355c626818dc58b29a02b9f6ab
Ryan Christensen <ryan.christensen20@gmail.com>
2017-05-25 15:12:34 -0700

    Adding animation to the ship.
    The ship changes between 4 colors.

--- ship.py
+++ ship.py
@@ -1,6 +1,11 @@
 import pygame
 from pygame.sprite import Sprite 
 
+def load_image(path):
+	"""Load the image and store into the image variable."""
+	image = pygame.image.load(path)
+	return image
+
 class Ship(Sprite):
 
 	def __init__(self, ai_settings, screen):
@@ -9,9 +14,16 @@ class Ship(Sprite):
 		self.screen = screen
 		self.ai_settings = ai_settings
 
-		# Load the ship image and get its rect.
-		self.image = pygame.image.load('images/ship_blue.png')
-		self.rect = self.image.get_rect()
+		# Load the ship images and get its rect.
+		self.images = []
+		self.images.append(load_image('images/ship_red.png'))
+		self.images.append(load_image('images/ship_green.png'))
+		self.images.append(load_image('images/ship_blue.png'))
+		self.images.append(load_image('images/ship_yellow.png'))
+		self.index = 0
+		self.image = self.images[self.index]
+		self.rect = pygame.Rect(0, 0, 64, 64)
+
 		self.screen_rect = screen.get_rect()
 
 		# Start each new ship at the bottom center of the screen.
@@ -30,7 +42,21 @@ class Ship(Sprite):
 		self.center = self.screen_rect.centerx
 
 	def update(self):
-		"""Update the ship's position based on the movement flag."""
+		"""
+			Update the ship image creating animation.
+			Update the ship's position based on the movement flag.
+		"""
+		# Animation for the ship to change colors!
+		for count in range(1000):
+			if count == 999:
+				self.index +=1
+				if self.index >= 3:
+					self.index = 0
+				self.image = self.images[self.index]
+				count = 0
+			else:
+				count += 1
+
 		# Update the ship's center value, not the rect.
 		if self.moving_right and self.rect.right < self.screen_rect.right:
 			self.center += self.ai_settings.ship_speed_factor
